58edf744a7d4762bbe176403abc9e86a3068d5a2
Jerry Lee <oldratlee@gmail.com>
2025-06-26 01:02:00 +0800

    refactor: extract methods from main

--- src/main/kotlin/com/example/scf/StringConstantFinder.kt
+++ src/main/kotlin/com/example/scf/StringConstantFinder.kt
@@ -14,69 +14,66 @@ import java.nio.file.Paths
 fun main(args: Array<String>) {
     if (args.size != 1) throw IllegalArgumentException("only one argument project root")
 
-    val projRootPath = args[0]
-    val projectRoot = ParserCollectionStrategy(ParserConfiguration().setLanguageLevel(JAVA_17))
-        .collect(Paths.get(projRootPath))
+    val compilationUnitList = collectCompilationUnits(args[0])
 
-    val compilationUnitList: List<CompilationUnit> = projectRoot.sourceRoots
+    val duplicateStrLiteralInfos: List<GroupStrLiterals> = compilationUnitList
+        .flatMap { findAllStringLiterals(it) }
+        .groupBy { it.value }
+        // ℹ️ 过滤没有重复使用的字符串常量（只用了一次）
+        .filter { it.value.size > 2 }
+        .map { (key, value) -> GroupStrLiterals(key, value) }
+        // 按常量的重复次数倒排
+        .sortedByDescending { it.strLiterals.size }
+
+    duplicateStrLiteralInfos.print()
+}
+
+private fun findAllStringLiterals(cu: CompilationUnit, minLen: Int = 4): List<StrLiteral> =
+    cu.findAll(StringLiteralExpr::class.java)
+        .filter { it.value.length >= minLen }
+        .map { StrLiteral(it.value, it, cu) }
+
+fun collectCompilationUnits(projectRootPath: String, excludeTestDir: Boolean = true): List<CompilationUnit> =
+    ParserCollectionStrategy(ParserConfiguration().setLanguageLevel(JAVA_17))
+        .collect(Paths.get(projectRootPath)).sourceRoots
         .onEach { println("found ${it.root}") }
         // ℹ️ 不处理测试代码
-        .filterNot { it.root.endsWith("/src/test/java") }
+        .filterNot { excludeTestDir && it.root.endsWith("/src/test/java") }
         .flatMap { it.tryToParseParallelized() }
         .filter {
             if (!it.isSuccessful) println(it.problems)
             it.isSuccessful
-        }
-        .map { it.result.get() }
+        }.map { it.result.get() }
 
-    val duplicateStrLiteralInfos: List<Pair<String, List<StringLiteralInfo>>> = compilationUnitList
-        .flatMap { cu ->
-            cu.findAll(StringLiteralExpr::class.java)
-                // ℹ️ 不找 短的字符串常量
-                .filter { it.value.length >= 4 }
-                .map { StringLiteralInfo(it.value, it, cu) }
-        }
-        .groupBy { it.value }
-        // ℹ️ 过滤没有重复使用的字符串常量（只用了一次）
-        .filter { it.value.size > 1 }
-        .map { (key, value) -> key to value }
-        // 按常量的重复次数倒排
-        .sortedByDescending { it.second.size }
-
-    printStringLiteralInfo(duplicateStrLiteralInfos)
-}
-
-fun printStringLiteralInfo(duplicateStrLiteralInfos: List<Pair<String, List<StringLiteralInfo>>>) {
-    duplicateStrLiteralInfos.forEachIndexed { index, (v, infoList) ->
+fun List<GroupStrLiterals>.print() {
+    forEachIndexed { index, (v, infoList) ->
         System.out.printf(
             "[%3d/%3d](count: %2d) duplicate string literal \"%s\" at%n",
-            index + 1, duplicateStrLiteralInfos.size, infoList.size, v
+            index + 1, size, infoList.size, v
         )
         infoList.forEachIndexed { idx, (_, expr, cu) ->
             val p: Position = expr.begin.get()
             System.out.printf(
-                "  [%2d/%2d] %s:%s:%s%n",
+                "  [%2d/%2d] .(%s:%s):%s%n",
                 idx + 1, infoList.size, cu.storage.get().path.fileName, p.line, p.column
             )
         }
     }
 }
 
-data class StringLiteralInfo(
+/**
+ * String Literals with same string value
+ */
+data class GroupStrLiterals(
+    val value: String,
+    val strLiterals: List<StrLiteral>
+)
+
+/**
+ * String Literal, contains related infos.
+ */
+data class StrLiteral(
     val value: String,
     val stringLiteralExpr: StringLiteralExpr,
     val cu: CompilationUnit
 )
-
-
-val BLACK_LIST = setOf(
-    "true", "null",
-    "body", "header", "etag",
-    "type", "version",
-    "unknown", "staging"
-)
-
-@Suppress("unused")
-val filter: (String) -> Boolean = {
-    !BLACK_LIST.contains(it) && it.length > 1
-}
