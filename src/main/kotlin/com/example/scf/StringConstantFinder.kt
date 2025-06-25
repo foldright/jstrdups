1e17cfddb0a654c8a2ec58cadfa361c735a26ab4
Jerry Lee <oldratlee@gmail.com>
2025-06-26 01:40:09 +0800

    refactor: extract method

--- src/main/kotlin/com/example/scf/StringConstantFinder.kt
+++ src/main/kotlin/com/example/scf/StringConstantFinder.kt
@@ -8,46 +8,47 @@ import com.github.javaparser.Position
 import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.expr.StringLiteralExpr
 import com.github.javaparser.utils.ParserCollectionStrategy
+import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.absolute
 
 
 fun main(args: Array<String>) {
     if (args.size != 1) throw IllegalArgumentException("only one argument project root")
-
-    val compilationUnitList = collectCompilationUnits(args[0])
-
-    val duplicateStrLiteralInfos: List<GroupStrLiterals> = compilationUnitList
-        .flatMap { findAllStringLiterals(it) }
-        .groupBy { it.value }
-        // ℹ️ 过滤没有重复使用的字符串常量（只用了一次）
-        .filter { it.value.size > 2 }
-        .map { (key, value) -> GroupStrLiterals(key, value) }
-        // 按常量的重复次数倒排
-        .sortedByDescending { it.strLiterals.size }
-
-    duplicateStrLiteralInfos.print()
+    val projectRootPath: Path = args[0].toNormalizePath()
+    val compilationUnitList = collectCompilationUnits(projectRootPath)
+    compilationUnitList.findDuplicateStrLiteralInfos().print()
 }
 
-private fun findAllStringLiterals(cu: CompilationUnit, minLen: Int = 4): List<StrLiteral> =
-    cu.findAll(StringLiteralExpr::class.java)
-        .filter { it.value.length >= minLen }
-        .map { StrLiteral(it.value, it, cu) }
+private fun String.toNormalizePath(): Path = Paths.get(this).absolute().normalize()
 
-fun collectCompilationUnits(projectRootPath: String, excludeTestDir: Boolean = true): List<CompilationUnit> =
+private fun collectCompilationUnits(projectRootPath: Path, excludeTestDir: Boolean = true): List<CompilationUnit> =
     ParserCollectionStrategy(ParserConfiguration().setLanguageLevel(JAVA_17))
-        .collect(Paths.get(projectRootPath).absolute().normalize())
+        .collect(projectRootPath)
         .sourceRoots
         .onEach { println("found ${it.root}") }
         // ℹ️ 不处理测试代码
         .filterNot { excludeTestDir && it.root.endsWith("/src/test/java") }
         .flatMap { it.tryToParseParallelized() }
-        .filter {
-            if (!it.isSuccessful) println(it.problems)
-            it.isSuccessful
-        }.map { it.result.get() }
+        .filter { it.result.isPresent }
+        .map { it.result.get() }
 
-fun List<GroupStrLiterals>.print() {
+private fun List<CompilationUnit>.findDuplicateStrLiteralInfos(
+    duplicateThreshold: Int = 2
+): List<GroupStrLiterals> = flatMap { it.findAllStringLiterals() }
+    .groupBy { it.value }
+    // ℹ️ 过滤没有重复使用的字符串常量（只用了一次）
+    .filter { it.value.size >= duplicateThreshold }
+    .map { (key, value) -> GroupStrLiterals(key, value) }
+    // 按常量的重复次数倒排
+    .sortedByDescending { it.strLiterals.size }
+
+private fun CompilationUnit.findAllStringLiterals(minLen: Int = 4): List<StrLiteral> =
+    findAll(StringLiteralExpr::class.java)
+        .filter { it.value.length >= minLen }
+        .map { StrLiteral(it.value, it, this) }
+
+private fun List<GroupStrLiterals>.print() {
     forEachIndexed { index, (v, infoList) ->
         System.out.printf(
             "[%3d/%3d](count: %2d) duplicate string literal \"%s\" at%n",
