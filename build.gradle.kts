49d042997c784ad9623d50716ca3da22d7d1210a
Jerry Lee <oldratlee@gmail.com>
2025-07-06 03:30:38 +0800

    add AutoComplete

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
   kotlin("jvm") version kotlinVersion
   kotlin("kapt") version kotlinVersion
   application
+  distribution
   id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -31,8 +32,14 @@ kotlin { jvmToolchain(8) }
 
 tasks.test { useJUnitPlatform() }
 
+
+val appName = "jstrdups"
 val mainClassName = "io.foldright.dslf.DuplicateStringLiteralFinder"
-application { mainClass.set(mainClassName) }
+application {
+  applicationName = appName
+  mainClass.set(mainClassName)
+}
+
 tasks.withType<ShadowJar> {
   manifest { attributes["Main-Class"] = mainClassName }
   exclude("META-INF/native-image/")
@@ -40,4 +47,32 @@ tasks.withType<ShadowJar> {
     exclude(dependency("org.jetbrains:annotations:.*"))
   }
 }
-tasks.build { dependsOn(tasks.shadowJar) }
+
+val taskGenAutoComplete = "genAutoComplete"
+tasks.register<JavaExec>(taskGenAutoComplete) {
+  // The module path: typically the runtimeClasspath of your main source set
+  classpath = sourceSets["main"].runtimeClasspath
+  workingDir = buildDir
+  mainClass = "picocli.AutoComplete"
+  args = listOf(mainClassName)
+}
+
+distributions {
+  main {
+    contents {
+      into("etc/bash_completion.d") {
+        from(buildDir.resolve("${appName}_completion"))
+      }
+      into("zsh/site-functions") {
+        from(buildDir.resolve("${appName}_completion")) {
+          rename { "_$appName" }
+        }
+      }
+    }
+  }
+}
+
+tasks.distZip { dependsOn(taskGenAutoComplete) }
+tasks.distTar { dependsOn(taskGenAutoComplete) }
+
+tasks.build { dependsOn(tasks.shadowJar, taskGenAutoComplete) }
